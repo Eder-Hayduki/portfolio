419022de0adf8b931e613e8ad3b143e70285bcb8
Eder-Hayduki <hayduki.corp@gmail.com>
2021-03-09 21:15:03 -0300

    portfolio

---
+++ js/script.js
@@ -0,0 +1,7 @@
+//passamos o resultado de uma função para outra função.
+document.querySelector(".hamburguer").addEventListener("click", () => 
+   document.querySelector(".container").classList.toggle("show-menu")
+    );
+
+
+
